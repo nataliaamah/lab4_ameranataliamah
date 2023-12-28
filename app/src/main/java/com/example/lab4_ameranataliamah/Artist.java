cdbbb6a34b6687e77db1be25969ac896dec679ad
nataliaamah <ameranatalia@gmail.com>
2023-12-28 21:59:29 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lab4_ameranataliamah/Artist.java
@@ -0,0 +1,20 @@
+package com.example.lab4_ameranataliamah;
+
+public class Artist {
+    String artistId;
+    String artistName;
+    public Artist(){}
+
+    public Artist(String artistId, String artistName) {
+        this.artistId = artistId;
+        this.artistName = artistName;
+    }
+
+    public String getArtistId() {
+        return artistId;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+}
